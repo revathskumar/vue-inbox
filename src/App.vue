db5d3097879372467f7e82ca72928fc19e2ed016
Revath S Kumar <rsk@revathskumar.com>
2016-02-26 02:00:57 +0530

    Adding layout and import css
    Also setup routes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app. To get started, take a look at the
-      <a href="https://github.com/vuejs-templates/webpack#folder-structure" target="_blank">README</a>
-      of this template. If you have any issues with the setup, please file an issue at this template's repository.
-    </p>
-    <p>
-      For advanced configurations, checkout the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Vue Mail</span>
+    <nav class="mdl-navigation">
+      <a v-link="{ path: '/inbox' }" class="mdl-navigation__link" href="#inbox"> Inbox </a>
+      <a v-link="{ path: '/drafts' }" class="mdl-navigation__link" href="#drafts"> Drafts </a>
+      <a v-link="{ path: '/sent' }" class="mdl-navigation__link" href="#sent"> Sent Items </a>
+      <a v-link="{ path: '/all' }" class="mdl-navigation__link" href="#all"> All Mails </a>
+    </nav>
   </div>
+  <main class="mdl-layout__content">
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
@@ -29,28 +22,3 @@ export default {
   }
 }
 </script>
-
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App'
+import Inbox from './Inbox'
+import Drafts from './Drafts'
+import SentItems from './SentItems'
+import All from './All'
+import VueRouter from 'vue-router'
+
+import 'material-design-lite/material.css'
+import 'material-design-lite/material.min.js'
+import 'material-design-icons/iconfont/material-icons.css'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+router.map({
+  '/': {
+    component: Inbox
+  },
+  '/inbox': {
+    component: Inbox
+  },
+  '/drafts': {
+    component: Drafts
+  },
+  '/sent': {
+    component: SentItems
+  },
+  '/all': {
+    component: All
+  }
+})
 
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+router.start(App, '#app')
