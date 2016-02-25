a4c7ad64f77eb42a0a6a7cc3d4b3b0f2fbad2c95
Revath S Kumar <rsk@revathskumar.com>
2016-02-26 04:47:57 +0530

    Adding floating button for compose

---
+++ src/components/Create.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-button--floating-action">
+    <i class="material-icons">add</i>
+  </button>
+</template>
+
+<style>
+.mdl-button--floating-action {
+  bottom:25px;
+  padding-top:24px;
+  position:fixed !important;
+  right:24px;
+  z-index:998;
+}
+</style>
