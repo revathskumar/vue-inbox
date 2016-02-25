2da522d3a957909a93909204e0fd897bb6e48eab
Revath S Kumar <rsk@revathskumar.com>
2016-02-26 04:47:07 +0530

    Config change to fix issue with github pages

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist/static'),
-    publicPath: '/static/',
+    publicPath: '/vue-inbox/static/',
     filename: '[name].js'
   },
   resolve: {
