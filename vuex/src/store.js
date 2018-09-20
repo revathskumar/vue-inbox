3c81ef60ff43c213386edb31b476b153ea5e1c69
Revath S Kumar <rsk@revathskumar.com>
2018-09-20 16:09:28 +0530

    loading inbox with vuex

--- vuex/src/store.js
+++ vuex/src/store.js
@@ -6,11 +6,45 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     inbox: {
+      uiState: 'LOADING',
       mails: [],
-    }
+      error: {},
+    },
   },
   mutations: {
-
+    setInboxFields(state, payload) {
+      state.inbox = {
+        ...state.inbox,
+        ...payload
+      }
+    }
   },
-  actions: {}
+  getters: {
+    isInboxLoading(state) {
+      return state.inbox.uiState === 'LOADING';
+    },
+    getInboxMails(state) {
+      return state.inbox.mails;
+    },
+    getInboxError(state) {
+      return state.inbox.error;
+    }
+  },
+  actions: {
+    async fetchMails({ commit }) {
+      commit('setInboxFields', { uiState: "LOADING" });
+      try {
+        const mails = await fetch('https://jsonplaceholder.typicode.com/posts')
+          .then((res) => {
+            if (res.status >= 400) {
+              throw new Error(res.statusText);
+            }
+            return res.json();
+          });
+        commit('setInboxFields', { mails, uiState: "LOADED" });
+      } catch (err) {
+        commit('setInboxFields', { uiState: "LOADED", error: { message: err.message } });
+      }
+    }
+  }
 });
--- vuex/src/views/Inbox.vue
+++ vuex/src/views/Inbox.vue
@@ -1,21 +1,47 @@
 <template>
   <section class="section">
     <div class="container is-fluid">
-      <div v-if="mails.length === 0" class="notification is-primary">
-        No mails in inbox
+      <div v-if="isInboxLoading">
+        Your mails are Loading...
+      </div>
+      <div v-else>
+        <div v-if="getInboxError.message" class="notification is-danger">
+          {{ getInboxError.message }}
+        </div>
+        <div v-else>
+          <div v-if="getInboxMails.length === 0" class="notification is-primary">
+            No mails in inbox
+          </div>
+          <ul v-if="getInboxMails.length > 0">
+            <li v-bind:key="mail.id" v-for="mail in getInboxMails">
+              <div class="box">
+                <div class="content">
+                  <h6>{{ mail.title }}</h6>
+                  <p>{{ mail.body }}</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
 export default {
   name: 'Inbox',
   computed: {
-    mails() {
-      console.log(this.$store);
-      return this.$store.state.inbox.mails;
-    },
+    ...mapGetters(['getInboxMails', 'isInboxLoading', 'getInboxError']),
+  },
+  methods: {
+    ...mapActions(['fetchMails']),
+  },
+  mounted() {
+    this.fetchMails();
   },
 };
 </script>
