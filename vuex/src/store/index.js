a5330fd3df24db42c6a5b38be62e37685dbe0cd9
Revath S Kumar <rsk@revathskumar.com>
2018-09-20 19:58:11 +0530

    better app structure for modules

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import { inbox } from "./modules/inbox";
+import { mutations } from "./mutations";
+import { actions } from "./actions";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     inbox,
-  }
+  },
+  mutations,
+  actions,
 });
