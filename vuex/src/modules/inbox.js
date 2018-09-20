5ac39c790a045b4272e04a0e437341664d067b9a
Revath S Kumar <rsk@revathskumar.com>
2018-09-20 19:48:31 +0530

    using modules

---
+++ vuex/src/modules/inbox.js
@@ -0,0 +1,45 @@
+export const inbox = {
+  state: {
+    inbox: {
+      uiState: 'LOADING',
+      mails: [],
+      error: {},
+    },
+  },
+  mutations: {
+    setInboxFields(state, payload) {
+      state.inbox = {
+        ...state.inbox,
+        ...payload
+      }
+    }
+  },
+  getters: {
+    isInboxLoading(state) {
+      return state.inbox.uiState === 'LOADING';
+    },
+    getInboxMails(state) {
+      return state.inbox.mails;
+    },
+    getInboxError(state) {
+      return state.inbox.error;
+    }
+  },
+  actions: {
+    async fetchMails({ commit }) {
+      commit('setInboxFields', { uiState: "LOADING" });
+      try {
+        const mails = await fetch('https://jsonplaceholder.typicode.com/posts')
+          .then((res) => {
+            if (res.status >= 400) {
+              throw new Error(res.statusText);
+            }
+            return res.json();
+          });
+        commit('setInboxFields', { mails, uiState: "LOADED" });
+      } catch (err) {
+        commit('setInboxFields', { uiState: "LOADED", error: { message: err.message } });
+      }
+    }
+  }
+}
